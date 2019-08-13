210da0781cb1eeef4ac094b938d392cd5bb98477
zhangd <d.zhang.1@elsevier.com>
2019-08-13 11:20:52 +0800

    test fork

--- factory/src/main/java/com/gpvip/pattern/factory/HuaweiPhone.java
+++ factory/src/main/java/com/gpvip/pattern/factory/HuaweiPhone.java
@@ -3,6 +3,6 @@ package com.gpvip.pattern.factory;
 public class HuaweiPhone implements Phone {
 
     public void sendMessage() {
-        System.out.println("This message is sent by huawei.");
+        System.out.println("来自华为的消息.");
     }
 }
