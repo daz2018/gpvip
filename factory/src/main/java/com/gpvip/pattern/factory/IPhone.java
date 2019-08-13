a074f17052f0a172be1b9b36d60d20c18d785894
dea09 <54050428+dea09@users.noreply.github.com>
2019-08-13 12:12:39 +0800

    test commit
    test comit

--- factory/src/main/java/com/gpvip/pattern/factory/IPhone.java
+++ factory/src/main/java/com/gpvip/pattern/factory/IPhone.java
@@ -3,6 +3,6 @@ package com.gpvip.pattern.factory;
 public class IPhone implements Phone {
 
     public void sendMessage() {
-        System.out.println("This message is sent by iphone!");
+        System.out.println("来自 iphone的消息!");
     }
 }
