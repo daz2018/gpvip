ef7431ea731a1d9184353a3663e668577d4897a2
zhangd <d.zhang.1@elsevier.com>
2019-08-13 11:24:14 +0800

    test fork02 from daz2018

--- factory/src/main/java/com/gpvip/pattern/factory/HuaweiPhone.java
+++ factory/src/main/java/com/gpvip/pattern/factory/HuaweiPhone.java
@@ -3,6 +3,6 @@ package com.gpvip.pattern.factory;
 public class HuaweiPhone implements Phone {
 
     public void sendMessage() {
-        System.out.println("来自华为的消息.");
+        System.out.println("来自华为的消息！");
     }
 }
